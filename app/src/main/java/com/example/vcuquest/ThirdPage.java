b1c39469be660ef2ce8a0e02ca7a0e75b2d48896
Alex <debordna@mymail.vcu.edu>
2020-02-16 17:19:37 -0500

    completed page 3 - need to implement passage to page 4

--- app/src/main/java/com/example/vcuquest/ThirdPage.java
+++ app/src/main/java/com/example/vcuquest/ThirdPage.java
@@ -2,15 +2,97 @@ package com.example.vcuquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ThirdPage extends AppCompatActivity {
 
+
+    Button nextPage;
+    Button submit;
+    TextView objectiveText;
+    TextView vcuQuestion;
+    TextView percentage;
+    EditText answer;
+    ProgressBar progress;
+    ImageButton mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_page);
+
+        nextPage = (Button) findViewById(R.id.nextButton);
+        objectiveText = (TextView) findViewById(R.id.objectiveText);
+        vcuQuestion = (TextView) findViewById(R.id.vcusign);
+        percentage = (TextView) findViewById(R.id.percent);
+        progress  = (ProgressBar) findViewById(R.id.progressBar);
+        mapButton = (ImageButton) findViewById(R.id.mapButton);
+        answer = (EditText) findViewById(R.id.answer);
+
+        nextPage.setVisibility(View.INVISIBLE);
+
+        answer.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if (answer.getText().toString().equalsIgnoreCase("the trustees"))
+                {
+                    nextPage.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    System.out.println("That's incorrect! Try again! (Hint: 'Ten' = 10");
+                    nextPage.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityThree();
+            }
+        });
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMapActivity();
+
+            }
+        });
+    }
+
+    private void openActivityThree()
+    {
+        Intent intent = new Intent(this, ThirdPage.class);
+        startActivity(intent);
+    }
+
+    private void goToMapActivity()
+    {
+        Intent intent = new Intent(this, MapPage.class);
+        startActivity(intent);
     }
 }
+
 
